365e6061198903dace4b12f8607ca663109c8a84
wthee <secretzqw@gmail.com>
2020-09-24 17:08:21 +0800

    Modified toast text

--- src/main/java/cn/wthee/pcrtool/utils/Constants.kt
+++ src/main/java/cn/wthee/pcrtool/utils/Constants.kt
@@ -69,7 +69,7 @@ object Constants {
     const val NOTICE_TITLE_ERROR = "数据库信息获取失败"
     const val NOTICE_TOAST_SUCCESS = "数据库更新完成"
     const val NOTICE_TOAST_NO_FILE = "数据库文件丢失，请稍后重试~"
-    const val NOTICE_TOAST_TITLE_DB_DOWNLOAD = "下载进度请查看通知栏"
+    const val NOTICE_TOAST_TITLE_DB_DOWNLOAD = "数据下载中\n请稍等..."
     const val NOTICE_TOAST_TIMEOUT = "若主页内容未正常显示，请点击${BTN_OPERATE_FORCE_UPDATE_DB}，下载最新数据库文件"
     const val NOTICE_TOAST_CHANGE = "数据库切换中......"
     const val NOTICE_TOAST_TODO = "功能暂未实装"
