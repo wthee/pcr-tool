6f537cd5d799ee3cd8525bca8d24461dc611a6a6
wthee <secretzqw@gmail.com>
2020-12-03 17:52:31 +0800

    1.优化角色排名页面

--- src/main/java/cn/wthee/pcrtool/adapters/CharacterLeaderAdapter.kt
+++ src/main/java/cn/wthee/pcrtool/adapters/CharacterLeaderAdapter.kt
@@ -10,6 +10,8 @@ import cn.wthee.pcrtool.MyApplication
 import cn.wthee.pcrtool.R
 import cn.wthee.pcrtool.data.model.LeaderboardInfo
 import cn.wthee.pcrtool.databinding.ItemLeaderBinding
+import cn.wthee.pcrtool.utils.ResourcesUtil
+import coil.load
 
 
 class CharacterLeaderAdapter :
@@ -37,9 +39,35 @@ class CharacterLeaderAdapter :
                         MyApplication.context,
                         R.anim.anim_translate_y
                     )
-                level.text = data.name
+                icon.load(data.icon) {
+                    placeholder(R.drawable.unknown_gray)
+                    error(R.drawable.unknown_gray)
+                }
+//                name.text = data.name
+                all.text = data.all
+                all.setTextColor(getColor(data.all))
+                pvp.text = data.pvp
+                pvp.setTextColor(getColor(data.pvp))
+                clan.text = data.clan
+                clan.setTextColor(getColor(data.clan))
+                tower.text = data.tower
+                tower.setTextColor(getColor(data.tower))
+
             }
         }
+
+        fun getColor(lv: String) = ResourcesUtil.getColor(
+            when (lv) {
+                "SSS" -> R.color.color_rank_18
+                "SS" -> R.color.color_rank_11_17
+                "S" -> R.color.color_rank_7_10
+                "A" -> R.color.color_rank_4_6
+                "B" -> R.color.color_rank_2_3
+                "C" -> R.color.cool_apk
+                else -> R.color.cool_apk
+            }
+        )
+
     }
 
 }
--- src/main/java/cn/wthee/pcrtool/ui/tool/leader/LeaderFragment.kt
+++ src/main/java/cn/wthee/pcrtool/ui/tool/leader/LeaderFragment.kt
@@ -11,10 +11,12 @@ import cn.wthee.pcrtool.R
 import cn.wthee.pcrtool.adapters.CharacterLeaderAdapter
 import cn.wthee.pcrtool.data.MyAPIRepository
 import cn.wthee.pcrtool.databinding.FragmentToolLeaderBinding
+import cn.wthee.pcrtool.enums.Response
 import cn.wthee.pcrtool.ui.main.CharacterViewModel
 import cn.wthee.pcrtool.utils.FabHelper
 import cn.wthee.pcrtool.utils.InjectorUtil
 import cn.wthee.pcrtool.utils.ResourcesUtil
+import cn.wthee.pcrtool.utils.ToastUtil
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.launch
 
@@ -34,13 +36,18 @@ class LeaderFragment : Fragment() {
         binding = FragmentToolLeaderBinding.inflate(inflater, container, false)
         MainScope().launch {
             val list = MyAPIRepository.getLeader()
-            val adapter = CharacterLeaderAdapter()
-            binding.listLevel.adapter = adapter
-            Log.e("test", list.data[0].name)
-            adapter.submitList(list.data)
+            if (list.status == Response.SUCCESS) {
+                val adapter = CharacterLeaderAdapter()
+                binding.listLevel.adapter = adapter
+                Log.e("test", list.data[0].name)
+                adapter.submitList(list.data)
+            } else {
+                ToastUtil.short(list.message)
+            }
+
         }
         //设置头部
-        binding.toolLevel.apply {
+        binding.toolLeader.apply {
             toolIcon.setImageDrawable(ResourcesUtil.getDrawable(R.drawable.ic_leader))
             toolTitle.text = getString(R.string.tool_leader)
         }
