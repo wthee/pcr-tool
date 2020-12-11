d43cd512e6792d139c2767d3b584820e9c496f41
wthee <secretzqw@gmail.com>
2020-12-11 16:54:41 +0800

    1.优化日历效果

--- src/main/java/cn/wthee/pcrtool/ui/tool/calendar/CalendarFragment.kt
+++ src/main/java/cn/wthee/pcrtool/ui/tool/calendar/CalendarFragment.kt
@@ -1,10 +1,6 @@
 package cn.wthee.pcrtool.ui.tool.calendar
 
-import android.graphics.*
-import android.graphics.drawable.BitmapDrawable
-import android.graphics.drawable.Drawable
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -15,12 +11,10 @@ import cn.wthee.pcrtool.data.network.MyAPIRepository
 import cn.wthee.pcrtool.data.network.model.CalendarDay
 import cn.wthee.pcrtool.databinding.FragmentToolCalendarBinding
 import cn.wthee.pcrtool.utils.FabHelper
-import cn.wthee.pcrtool.utils.ResourcesUtil
 import cn.wthee.pcrtool.utils.ToastUtil
 import cn.wthee.pcrtool.utils.ToolbarUtil
+import com.applandeo.materialcalendarview.CalendarUtils.getDrawableText
 import com.applandeo.materialcalendarview.EventDay
-import com.applandeo.materialcalendarview.listeners.OnCalendarPageChangeListener
-import com.applandeo.materialcalendarview.listeners.OnDayClickListener
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.launch
 import java.util.*
@@ -71,32 +65,24 @@ class CalendarFragment : Fragment() {
             minCal = Calendar.getInstance()
             minCal.set(2020, 6 - 1, 6)
             setMinimumDate(minCal)
-            setOnDayClickListener(object : OnDayClickListener {
-                override fun onDayClick(eventDay: EventDay) {
-                    showDayEvents(eventDay.calendar)
-                }
-            })
+            setOnDayClickListener { eventDay -> showDayEvents(eventDay.calendar) }
             //月份切换监听
-            setOnForwardPageChangeListener(object : OnCalendarPageChangeListener {
-                override fun onChange() {
-                    mMonth++
-                    if (mMonth > 12) {
-                        mMonth = 1
-                        mYear++
-                    }
-                    addIcon(mYear, mMonth)
+            setOnForwardPageChangeListener {
+                mMonth++
+                if (mMonth > 12) {
+                    mMonth = 1
+                    mYear++
                 }
-            })
-            setOnPreviousPageChangeListener(object : OnCalendarPageChangeListener {
-                override fun onChange() {
-                    mMonth--
-                    if (mMonth < 1) {
-                        mMonth = 12
-                        mYear--
-                    }
-                    addIcon(mYear, mMonth)
+                addIcon(mYear, mMonth)
+            }
+            setOnPreviousPageChangeListener {
+                mMonth--
+                if (mMonth < 1) {
+                    mMonth = 12
+                    mYear--
                 }
-            })
+                addIcon(mYear, mMonth)
+            }
         }
 
         //回到今天
@@ -108,7 +94,7 @@ class CalendarFragment : Fragment() {
             getEvents(object : OnLoadFinish {
                 override fun finish(maxDate: String) {
                     //设置最大值
-                    val maxCal = Calendar.getInstance()
+                    maxCal = Calendar.getInstance()
                     val date = maxDate.split("/")
                     maxCal.set(date[0].toInt(), date[1].toInt() - 1, date[2].toInt())
                     binding.calendarView.setMaximumDate(maxCal)
@@ -168,7 +154,6 @@ class CalendarFragment : Fragment() {
 
     //添加事件图标
     private fun addIcon(year: Int, month: Int) {
-        Log.e("addIcon", "$year,$month")
         val eventData = events.filter {
             it.date.split("/")[0].toInt() == year
                     && it.date.split("/")[1].toInt() == month
@@ -186,7 +171,12 @@ class CalendarFragment : Fragment() {
             events.add(
                 EventDay(
                     calendar,
-                    getDrawableText(eventCount.toString(), R.color.colorPrimaryDark)
+                    getDrawableText(
+                        requireContext(),
+                        eventCount.toString(),
+                        R.color.colorPrimaryDark,
+                        10
+                    )
                 )
             )
         }
@@ -196,26 +186,6 @@ class CalendarFragment : Fragment() {
 
     interface OnLoadFinish {
         fun finish(maxDate: String)
-    }
-
-    //文本转 drawable
-    private fun getDrawableText(
-        text: String,
-        colorId: Int
-    ): Drawable {
-        val bitmap = Bitmap.createBitmap(48, 48, Bitmap.Config.ARGB_8888)
-        val canvas = Canvas(bitmap)
-        val paint = Paint(Paint.ANTI_ALIAS_FLAG)
-        paint.typeface = Typeface.create(Typeface.DEFAULT, Typeface.BOLD)
-        paint.color = ResourcesUtil.getColor(colorId)
-        val scale: Float = resources.displayMetrics.density
-        paint.textSize = 10 * scale
-        val bounds = Rect()
-        paint.getTextBounds(text, 0, text.length, bounds)
-        val x = (bitmap.width - bounds.width()) / 2f
-        val y = (bitmap.height + bounds.height()) / 2f
-        canvas.drawText(text, x, y, paint)
-        return BitmapDrawable(resources, bitmap)
     }
 
 
