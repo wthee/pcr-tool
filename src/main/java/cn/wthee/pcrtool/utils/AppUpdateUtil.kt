870372c4c1f4ae4e5cb28bdb8489a15730d2eb88
wthee <3116728602@qq.com>
2021-02-10 10:12:53 +0800

    修复已知 BUG

--- src/main/java/cn/wthee/pcrtool/utils/AppUpdateUtil.kt
+++ src/main/java/cn/wthee/pcrtool/utils/AppUpdateUtil.kt
@@ -5,6 +5,8 @@ import cn.wthee.pcrtool.MainActivity
 import cn.wthee.pcrtool.R
 import cn.wthee.pcrtool.data.network.MyAPIRepository
 import cn.wthee.pcrtool.utils.ResourcesUtil.getString
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
 
 /**
  * 应用更新
@@ -20,14 +22,18 @@ object AppUpdateUtil {
                 val version = MyAPIRepository.getInstance().getAppUpdateNotice()
                 if (version.status == 0) {
                     if (version.data != null && version.data == true) {
-                        MainActivity.fabNotice.show()
+                        MainScope().launch {
+                            MainActivity.fabNotice.show()
+                        }
                     }
                 }
             } else {
                 ToastUtil.short(getString(R.string.network_error))
             }
         } catch (e: Exception) {
-            MainActivity.fabNotice.hide()
+            MainScope().launch {
+                MainActivity.fabNotice.hide()
+            }
             Log.e(Constants.LOG_TAG, e.message ?: "")
         }
 
