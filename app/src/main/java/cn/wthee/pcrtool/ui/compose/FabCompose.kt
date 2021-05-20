03af8f3af14d17d87923705fbd3d76fff7443e91
wthee <secretzqw@gmail.com>
2021-05-20 14:25:56 +0800

    颜色调整

--- app/src/main/java/cn/wthee/pcrtool/ui/compose/FabCompose.kt
+++ app/src/main/java/cn/wthee/pcrtool/ui/compose/FabCompose.kt
@@ -1,7 +1,6 @@
 package cn.wthee.pcrtool.ui.compose
 
 import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -11,6 +10,8 @@ import androidx.compose.material.FloatingActionButtonDefaults
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -33,9 +34,10 @@ fun FabCompose(
     defaultPadding: Boolean = true,
     onClick: () -> Unit
 ) {
+    val visible = remember { mutableStateOf(false) }
+    visible.value = text != ""
 
     val context = LocalContext.current
-    val fabAnim = animateFloatAsState(targetValue = if (text == "") 0f else 1f)
     var mModifier = modifier
     if (defaultPadding) {
         mModifier = modifier.navigationBarsPadding()
@@ -60,7 +62,7 @@ fun FabCompose(
             modifier = Modifier.padding(Dimen.fabPadding)
         ) {
             IconCompose(iconType.icon)
-            ExtendedAnimation(visible = fabAnim.value == 1f) {
+            ExtendedAnimation(visible = visible.value) {
                 Text(
                     text = text,
                     style = MaterialTheme.typography.subtitle2,
