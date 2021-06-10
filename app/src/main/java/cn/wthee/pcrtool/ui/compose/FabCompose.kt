b76aeba72d1a808eac2a5e01aeafd74d35585f43
wthee <secretzqw@gmail.com>
2021-06-10 13:29:49 +0800

    悬浮按钮调整

--- app/src/main/java/cn/wthee/pcrtool/ui/compose/FabCompose.kt
+++ app/src/main/java/cn/wthee/pcrtool/ui/compose/FabCompose.kt
@@ -1,17 +1,16 @@
 package cn.wthee.pcrtool.ui.compose
 
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.FloatingActionButtonDefaults
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -34,42 +33,43 @@ fun FabCompose(
     defaultPadding: Boolean = true,
     onClick: () -> Unit
 ) {
-    val visible = remember { mutableStateOf(false) }
-    visible.value = text != ""
-
     val context = LocalContext.current
-    var mModifier = modifier
-    if (defaultPadding) {
-        mModifier = modifier.navigationBarsPadding()
-    }
-    mModifier = if (text != "") {
-        mModifier.height(Dimen.fabSize)
+    val mModifier = if (defaultPadding) {
+        modifier.navigationBarsPadding()
     } else {
-        mModifier.size(Dimen.fabSize)
+        modifier
     }
 
     FloatingActionButton(
         onClick = onClick.vibrate {
             VibrateUtil(context).single()
         },
+        shape = CircleShape,
         elevation = FloatingActionButtonDefaults.elevation(defaultElevation = Dimen.fabElevation),
         backgroundColor = MaterialTheme.colors.onPrimary,
         contentColor = MaterialTheme.colors.primary,
-        modifier = mModifier,
+        modifier = mModifier.defaultMinSize(minWidth = Dimen.fabSize, minHeight = Dimen.fabSize),
     ) {
         Row(
             verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier.padding(Dimen.fabPadding)
-        ) {
-            IconCompose(iconType.icon)
-            ExtendedAnimation(visible = visible.value) {
-                Text(
-                    text = text,
-                    style = MaterialTheme.typography.subtitle2,
-                    textAlign = TextAlign.Center,
-                    modifier = Modifier.padding(end = Dimen.fabPadding)
-                )
+            modifier = if (text != "") {
+                Modifier.padding(start = Dimen.fabPadding)
+            } else {
+                Modifier
             }
+        ) {
+            IconCompose(iconType.icon, size = Dimen.fabIconSize)
+            Text(
+                text = text,
+                style = MaterialTheme.typography.subtitle2,
+                textAlign = TextAlign.Center,
+                modifier = if (text != "") {
+                    Modifier.padding(start = Dimen.fabPadding, end = Dimen.fabPadding)
+                } else {
+                    Modifier
+                }.animateContentSize(defaultTween())
+            )
         }
     }
+
 }
