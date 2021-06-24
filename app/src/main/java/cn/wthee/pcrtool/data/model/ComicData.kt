b0134c5ac13bd503be3e95b8cc2362d925d74f2b
wthee <3116728602@qq.com>
2021-06-24 23:57:21 +0800

    修复正式版漫画无法加载问题

--- app/src/main/java/cn/wthee/pcrtool/data/model/ComicData.kt
+++ app/src/main/java/cn/wthee/pcrtool/data/model/ComicData.kt
@@ -1,15 +1,10 @@
-package cn.wthee.pcrtool.data.db.entity
-
-import androidx.room.Entity
-import androidx.room.PrimaryKey
+package cn.wthee.pcrtool.data.model
 
 /**
  * 日服从接口获取
  * 国服官方 https://manga.bilibili.com/detail/mc28174
  */
-@Entity(tableName = "comic")
 data class ComicData(
-    @PrimaryKey
     val id: Int = -1,
     val title: String = "",
     val url: String = "",
