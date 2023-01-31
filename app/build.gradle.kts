28e19e25ce6c41887bc7e6f10172e10857cb6d00
wthee <secretzqw@gmail.com>
2023-01-31 20:59:14 +0800

    3.5.4 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ hilt {
 
 val composeVersion = "1.4.0-alpha05"
 val composeCompilerVersion = "1.4.0"
-val appVersionCode = 353
-val appVersionName = "3.5.3"
+val appVersionCode = 354
+val appVersionName = "3.5.4"
 val appId = "cn.wthee.pcrtool"
 
 android {
