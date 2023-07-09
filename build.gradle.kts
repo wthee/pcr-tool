5b999b78f2c3f34507867d0a9a82dc26974878b6
wthee <secretzqw@gmail.com>
2023-07-09 11:18:35 +0800

    3.6.5 release

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
-    val kotlinVersion by extra("1.8.21")
+    val kotlinVersion by extra("1.8.22")
     val hiltVersion by extra("2.46.1")
 
     repositories {
