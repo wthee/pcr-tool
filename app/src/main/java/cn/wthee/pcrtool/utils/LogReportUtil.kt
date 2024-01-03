13657effba50b8bc380baa851d46c35766a2fe70
wthee <secretzqw@gmail.com>
2024-01-03 14:23:07 +0800

    调整 bugly 日志模板

--- app/src/main/java/cn/wthee/pcrtool/utils/LogReportUtil.kt
+++ app/src/main/java/cn/wthee/pcrtool/utils/LogReportUtil.kt
@@ -2,6 +2,7 @@ package cn.wthee.pcrtool.utils
 
 import android.util.Log
 import cn.wthee.pcrtool.BuildConfig
+import cn.wthee.pcrtool.ui.MainActivity
 import com.tencent.bugly.crashreport.CrashReport
 import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.MainScope
@@ -18,11 +19,12 @@ object LogReportUtil {
     fun upload(e: Exception, msg: String) {
         MainScope().launch {
             if (BuildConfig.DEBUG) {
-                Log.e("LogReportUtil", msg + e.message)
+                Log.e("LogReportUtil", "$msg\n${e.message}")
             }
             //排除协程取消异常、SQLite异常后上传
             if (e !is CancellationException && !(e.message?:"").contains("no such table")) {
-                val exception = Exception("$msg\n${e.message}")
+                val exception =
+                    Exception("$msg; dbVersion: ${MainActivity.regionType.name}\n${e.message}")
                 exception.stackTrace = e.stackTrace
                 CrashReport.postCatchedException(exception)
             }
