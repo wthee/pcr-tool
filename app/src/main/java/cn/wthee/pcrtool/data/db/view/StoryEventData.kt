73a216b35f4449aa9174e1bc6ace9f308c80b888
wthee <secretzqw@gmail.com>
2025-08-28 15:26:44 +0800

    修复剧情活动标题未空导致查询失败的问题

--- app/src/main/java/cn/wthee/pcrtool/data/db/view/StoryEventData.kt
+++ app/src/main/java/cn/wthee/pcrtool/data/db/view/StoryEventData.kt
@@ -13,13 +13,13 @@ data class StoryEventData(
     @ColumnInfo(name = "original_event_id") val originalEventId: Int = -1,
     @ColumnInfo(name = "start_time") val startTime: String = "2020/01/01 00:00:00",
     @ColumnInfo(name = "end_time") val endTime: String = "2020/01/07 00:00:00",
-    @ColumnInfo(name = "title") val title: String = "???",
+    @ColumnInfo(name = "title") val title: String? = "???",
     @ColumnInfo(name = "unit_ids") val unitIds: String = "",
     @ColumnInfo(name = "boss_enemy_id") val bossEnemyId: Int = 0,
     @ColumnInfo(name = "boss_unit_id") val bossUnitId: Int = 0,
 ) {
 
-    fun getEventTitle() = title.deleteSpace
+    fun getEventTitle() = title?.deleteSpace ?: ""
 
 
     fun getUnitIdList(): List<Int> {
